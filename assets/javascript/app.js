178b413cb626d4046bad386adedc44e357f79ccf
Alex Pino <alex@Alexs-MacBook-Pro.local>
2019-03-23 13:01:42 -0400

    very basic psuedocoding outline

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+// -------------- PSUEDO CODE -------------- //
+
+// 1) Starting page - intro, instructions, login, start button
+// 2) User logs in, hits start
+// 3) Form pops up
+// 4) User fills out form
+// 5) Keywords from user input inform a query URL
+// 6) API cross-searches to find list of places
+// 7) Display list of places with their various criteria 
+// 8) When a user clicks a resturaunt take them to the place's website
