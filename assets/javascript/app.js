4fb9e91caa3bb24f681ba698969f1af1d386080b
Alex Pino <alex@Alexs-MacBook-Pro.local>
2019-03-24 22:20:14 -0400

    basic html started, changed allegra ids from camelcase to kabobcase

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,3 +8,26 @@
 // 6) API cross-searches to find list of places
 // 7) Display list of places with their various criteria 
 // 8) When a user clicks a resturaunt take them to the place's website
+
+
+// From Allegra's trials
+
+var baseURL = "https://developers.zomato.com/api/v2.1/search?entity_id=287&entity_type=city";
+var userSearch;
+
+function getData(e) {
+    e.preventDefault();
+    userSearch = $(".user-search").val().trim();
+    $.ajax({
+        url: `${baseURL}&q=${userSearch}`,
+        method: "GET",
+        headers: {
+            "accept": "application/json",
+            "user-key": "a78c76989a0d84745c0c782d502cd107"
+        }
+    }).then(function (response) {
+        console.log(response)
+    });
+}
+
+$("#submit-btn").on("click", getData);
